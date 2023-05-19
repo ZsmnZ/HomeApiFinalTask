b09ac185ce7c43723c00156e3daa2481ff6e9ba5
Smn <smnzarubin@yandex.ru>
2023-05-19 15:59:19 +0300

    Добавьте файлы проекта.

---
+++ HomeApi.Data/HomeApiContext.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using HomeApi.Data.Models;
+using Microsoft.EntityFrameworkCore;
+using DbContext = Microsoft.EntityFrameworkCore.DbContext;
+
+namespace HomeApi.Data
+{
+    public sealed class HomeApiContext : DbContext
+    {
+        public DbSet<Room> Rooms { get; set; }
+        public DbSet<Device> Devices { get; set; }
+        
+        public HomeApiContext(DbContextOptions<HomeApiContext> options)  : base(options)
+        {
+            //Database.EnsureDeleted();
+            Database.EnsureCreated();
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<Room>().ToTable("Rooms");
+            builder.Entity<Device>().ToTable("Devices");
+        }
+    }
+}
---
+++ HomeApi.Data/Queries/UpdateRoomQuery.cs
@@ -0,0 +1,9 @@
+﻿namespace HomeApi.Data.Queries
+{
+    public class UpdateRoomQuery
+    {
+        public string newName { get; set; }
+        public int newArea { get; set; }
+
+    }
+}
